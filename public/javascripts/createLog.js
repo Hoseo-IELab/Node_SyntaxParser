37f9d0d07d79b284c2222ba058c6d4c22eeb009d
seokii <dev.seokii@gmail.com>
2021-07-22 18:45:58 +0900

    20210722
    by minseok

---
+++ public/javascripts/createLog.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const fs = require('fs');
+
+const directoryPath = path.join(__dirname, '../../test');
+
+fs.readdir(directoryPath, function (err, files) {
+    //handling error
+    if (err) {
+        return console.log('Unable to scan directory: ' + err);
+    } 
+    //listing all files using forEach
+    files.forEach(function (file) {
+        // Do whatever you want to do with the file
+        console.log(file); 
+    });
+});
